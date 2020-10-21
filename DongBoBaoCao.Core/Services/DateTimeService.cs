945d4dfdc3ec689f80ad4ec93d5b76fb762edee2
Bui Sy Tu <tubs@tandan.com.vn>
2020-10-21 16:13:23 +0700

    update datetime service

--- DongBoBaoCao.Core/Services/DateTimeService.cs
+++ DongBoBaoCao.Core/Services/DateTimeService.cs
@@ -12,12 +12,38 @@ namespace DongBoBaoCao.Core.Services
             return startDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
         }
 
-        public string GetLastDateOfMonth (int month, int year)
+        public string GetStartDateOfMonth(int month, int year, string format)
+        {
+            var startDate = new DateTime(year, month, 1);
+            return startDate.ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        public string GetLastDateOfMonth(int month, int year)
         {
             var startDate = new DateTime(year, month, 1);
             var endDate = startDate.AddMonths(1).AddDays(-1);
 
             return endDate.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture);
+        }
+
+        public string GetLastDateOfMonth(int month, int year, string format)
+        {
+            var startDate = new DateTime(year, month, 1);
+            var endDate = startDate.AddMonths(1).AddDays(-1);
+
+            return endDate.ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        public DateTime StringToDateTime(string dateString, string format)
+        {
+            var provider = CultureInfo.InvariantCulture;
+            var result = DateTime.ParseExact(dateString, format, provider);
+            return result;
+        }
+
+        public string DateTimeToString(DateTime date, string format)
+        {
+            return date.ToString(format);
         }
     }
 }
